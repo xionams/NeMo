6fcdf60267685d7a38fb1045ff06a6ac2962d672
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-01-30 07:34:37 +0000

    Fix pip installation in jenkins; https://stackoverflow.com/questions/19548957/can-i-force-pip-to-reinstall-the-current-version
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- reinstall.sh
+++ reinstall.sh
@@ -34,7 +34,7 @@ else
     ${PIP} install build pytest-runner
     python -m build --no-isolation --wheel
     DIST_FILE=$(find ./dist -name "*.whl" | head -n 1)
-    ${PIP} install "${DIST_FILE}[all]"
+    ${PIP} install --force-reinstall --no-deps --no-cache-dir "${DIST_FILE}[all]"
 fi
 
 echo 'All done!'
